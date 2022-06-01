ccf7cc1c3f288dacc0b5a65361adfa61f44ad415
Andre Bandarra <andreban@google.com>
2022-06-01 16:22:45 +0100

    Adds message for enabling WebGPU

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,21 @@ import { Renderer } from './rendering/renderer';
 
 (async () => {
   const canvas = document.querySelector('#game-canvas') as HTMLCanvasElement;
+  const error = document.querySelector('#webgpu-not-supported') as HTMLDivElement;
   const inputState = new InputState(document);
-  const renderer = await Renderer.create(canvas);
-  const game = new JackGame();
-  const gameStart = performance.now();
-  const render = () => {
-    const updated = performance.now();
-    game.update(inputState, updated - gameStart);
-    game.render(renderer);
-    requestAnimationFrame(render);
-  };
-  render();
+  try {
+    const renderer = await Renderer.create(canvas);
+    const game = new JackGame();
+    const gameStart = performance.now();
+    const render = () => {
+      const updated = performance.now();
+      game.update(inputState, updated - gameStart);
+      game.render(renderer);
+      requestAnimationFrame(render);
+    };
+    render();
+  } catch(e) {
+    canvas.classList.toggle('hidden');
+    error.classList.toggle('hidden');
+  }
 })();
